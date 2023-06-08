70c16938e287538763696b690c1478ba8cb3a750
yuktnk <yuki0wl@gmail.com>
2023-06-08 22:36:26 +0900

    色やfontSizeなどconfigに定義し使えるようにしました

--- src/components/atoms/Card/RecipeCard.tsx
+++ src/components/atoms/Card/RecipeCard.tsx
@@ -6,13 +6,13 @@ interface RecipeCardProps {
 
 export const RecipeCard = ({ index, title, description }: RecipeCardProps) => {
   return (
-    <li className="list-none py-2 px-4 border-0 border-b-[1px] border-[#DCDBDD] border-solid flex max-w-[400px] box-border">
-      <p className="my-0 w-[18px] h-[18px] bg-[#CA3214] text-center rounded-full text-xs text-white leading-[18px]">
+    <li className="list-none py-2 px-4 border-0 border-b-[1px] border-Mauve-07 border-solid flex max-w-[400px] box-border">
+      <p className="text-Mauve-01 text-fs12 leading-[18px] text-center my-0 w-[18px] h-[18px] bg-Tomato-11 rounded-full">
         {index}
       </p>
       <div className="ml-2 flex-1">
-        <h3 className="text-[#1A1523] text-[14px] leading-[18px] my-0">{title}</h3>
-        <p className="text-[10px] leading-4 mb-0 mt-1 text-[#86848D]">{description}</p>
+        <h3 className="text-Mauve-12 text-fs14 leading-[18px] my-0">{title}</h3>
+        <p className="text-Mauve-10 text-fs10 leading-4 mb-0 mt-1">{description}</p>
       </div>
     </li>
   )
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,21 +1,32 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
       backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic": "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+    },
+    colors: {
+      "Mauve-12": "#1A1523", // Mauve/Light/12
+      "Mauve-10": "#86848D", // Mauve/Light/10
+      "Mauve-07": "#DCDBDD", // Mauve/Light/7
+      "Mauve-01": "#FDFCFD", // Mauve/Light/1
+      "Tomato-11": "#CA3214", // Tomato/Light/11
+    },
+    fontSize: {
+      fs10: "10px",
+      fs12: "12px",
+      fs14: "14px",
     },
   },
   plugins: [],
   corePlugins: {
-    preflight: false
-  }
+    preflight: false,
+  },
 }
