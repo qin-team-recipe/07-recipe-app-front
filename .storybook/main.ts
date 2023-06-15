1761a3e087f091072b2f2a7770c89f893d95f1ed
hidaken991018 <ed.hidaken@gmail.com>
2023-06-15 21:15:53 +0900

    chore: storybook パスエイリアス使用のため設定追加

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,25 +1,37 @@
-import type { StorybookConfig } from '@storybook/nextjs';
+import type { StorybookConfig } from "@storybook/nextjs"
+const path = require("path")
 const config: StorybookConfig = {
-  stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
+  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
   addons: [
-    '@storybook/addon-links',
-    '@storybook/addon-essentials',
-    '@storybook/addon-interactions',
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/addon-interactions",
     {
-      name: '@storybook/addon-postcss',
+      name: "@storybook/addon-postcss",
       options: {
         postcssLoaderOptions: {
-          implementation: require('postcss'),
+          implementation: require("postcss"),
         },
       },
     },
   ],
   framework: {
-    name: '@storybook/nextjs',
+    name: "@storybook/nextjs",
     options: {},
   },
   docs: {
-    autodocs: 'tag',
+    autodocs: "tag",
   },
-};
-export default config;
+  staticDirs: ["../public"],
+  webpackFinal: async (config) => {
+    // storybookでaliasを使うための設定
+    if (config.resolve) {
+      config.resolve.alias = {
+        ...config.resolve.alias,
+        "@": path.resolve(__dirname, "../src"),
+      }
+    }
+    return config
+  },
+}
+export default config
