22e649681b941b43872eb3c0f3f3b7da2d5a9c68
kyoichi <s206cg06k@yahoo.co.jp>
2023-06-16 09:51:37 +0900

    修正：tailwind設定変更（preflight: true）のためm-0は不要となり削除

--- src/components/atoms/FeatureChef/FeatureChef.tsx
+++ src/components/atoms/FeatureChef/FeatureChef.tsx
@@ -11,7 +11,7 @@ export const FeatureChef = (props: FeatureChefProps) => {
   return (
     <Link href={props.url} className="relative">
       <Image src={props.icon} width={148} height={220} alt="ChefIcon" className="rounded-2xl" />
-      <p className="absolute bottom-[11px] left-3 m-0 text-[20px] font-bold text-Mauve-01">{props.name}</p>
+      <p className="absolute bottom-[11px] left-3 text-[20px] font-bold text-Mauve-01">{props.name}</p>
     </Link>
   )
 }
