429e576858530fbdc26a5432d2b8164005687fb0
RyotaS <wstone.notsw@gmail.com>
2023-06-16 15:58:24 +1000

    fix:レビュー指摘対応1

--- src/components/atoms/LikeButton/LikeButton.tsx
+++ src/components/atoms/LikeButton/LikeButton.tsx
@@ -1,30 +1,32 @@
+"use client"
+
 import Image from "next/image"
 import { useState } from "react"
 
 import { ThemeIcon } from "@mantine/core"
 
 interface LikeButtonProps {
-  isLiked: boolean
+  isLikedProp: boolean
 }
 
-export const LikeButton = ({ isLiked }: LikeButtonProps) => {
-  const [state, setState] = useState(isLiked)
+export const LikeButton = ({ isLikedProp }: LikeButtonProps) => {
+  const [isLiked, setIsLiked] = useState(isLikedProp)
 
   const turnOnLike = () => {
-    setState(!state)
+    setIsLiked(!isLiked)
   }
   const turnOffLike = () => {
-    setState(!state)
+    setIsLiked(!isLiked)
   }
-  return state ? (
+  return isLiked ? (
     <>
-      <ThemeIcon radius="xl" size="xl" className="bg-Tomato-12 w-[60px] h-[60px]" onClick={turnOffLike}>
+      <ThemeIcon radius="xl" size="xl" className="bg-Tomato-12 w-[60px] h-[60px] cursor-pointer" onClick={turnOffLike}>
         <Image src="heart.svg" alt="ライク" width={36} height={36}></Image>
       </ThemeIcon>
     </>
   ) : (
     <>
-      <ThemeIcon radius="xl" size="xl" className="bg-Mauve-10 w-[60px] h-[60px]" onClick={turnOnLike}>
+      <ThemeIcon radius="xl" size="xl" className="bg-Mauve-10 w-[60px] h-[60px] cursor-pointer" onClick={turnOnLike}>
         <Image src="heart.svg" alt="ライク" width={36} height={36}></Image>
       </ThemeIcon>
     </>
