7f1b8196c9cbcb5da140b264f754b34f76bfb049
RyotaS <wstone.notsw@gmail.com>
2023-06-16 18:06:45 +1000

    fix:レビュー指摘対応2

--- src/components/atoms/RecipePreviewText/RecipePreviewText.tsx
+++ src/components/atoms/RecipePreviewText/RecipePreviewText.tsx
@@ -5,9 +5,9 @@ interface RecipePreviewTextProps {
 
 export const RecipePreviewText = ({ previewTitle, previewContent }: RecipePreviewTextProps) => {
   return (
-    <div className="flex flex-col items-start w-[160px] h-[46px] gap-[4px] m-0 p-0">
-      <p className="text-Mauve-12 text-fs12 font-bold leading-[18px]  m-0 p-0 line-clamp-2">{previewTitle}</p>
-      <p className="text-Mauve-11 text-fs10 font-normal leading-[12px]  m-0 p-0 line-clamp-1">{previewContent}</p>
+    <div className="flex flex-col items-start w-[160px] h-[46px]">
+      <p className="text-Mauve-12 text-fs12 font-bold leading-3 line-clamp-2">{previewTitle}</p>
+      <p className="text-Mauve-11 text-fs10 font-normal leading-[10px] line-clamp-1">{previewContent}</p>
     </div>
   )
 }
