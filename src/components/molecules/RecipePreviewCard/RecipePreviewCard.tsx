5cbdefc96433fb041cf937191cfe1a02364356b6
RyotaS <wstone.notsw@gmail.com>
2023-06-28 21:22:59 +1000

    feat :+1: : FavariteCountコンポーネントをRecipePreviewCardコンポーネントに追加
    Issue #

--- src/components/molecules/RecipePreviewCard/RecipePreviewCard.tsx
+++ src/components/molecules/RecipePreviewCard/RecipePreviewCard.tsx
@@ -1,21 +1,26 @@
 import Link from "next/link"
 
+import { FavoriteCount } from "../../atoms/FavoriteCount/FavoriteCount"
 import { RecipePreviewPicture } from "../../atoms/RecipePreviewPicture/RecipePreviewPicture"
 import { RecipePreviewText } from "../../atoms/RecipePreviewText/RecipePreviewText"
 
-interface RecipePreviewCardProps {
+type RecipePreviewCardProps = {
   recipe: {
     index: number
     url: string
     title: string
     content: string
+    count: number
   }
 }
 
 export const RecipePreviewCard = ({ recipe }: RecipePreviewCardProps) => {
   return (
-    <div className="grid gap-2 justify-center w-[173px] h-[214]">
+    <div className="grid gap-2 justify-center w-[194px] h-[248px]">
       <Link href="">
+        <div className="relative">
+          <FavoriteCount count={recipe.count}></FavoriteCount>
+        </div>
         <RecipePreviewPicture index={recipe.index} url={recipe.url}></RecipePreviewPicture>
       </Link>
       <RecipePreviewText previewTitle={recipe.title} previewContent={recipe.content}></RecipePreviewText>
@@ -23,4 +28,3 @@ export const RecipePreviewCard = ({ recipe }: RecipePreviewCardProps) => {
   )
 }
 //TODO: ボタン押下時の処理
-//TODO: Likeの数を表示するコンポーネントを追加する
