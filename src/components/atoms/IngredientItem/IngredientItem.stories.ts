674edec4e3d19358540c78fce97bd5c6321d2d01
yuktnk <yuki0wl@gmail.com>
2023-06-29 17:32:38 +0900

    IngredientItemを拡張

--- src/components/atoms/IngredientItem/IngredientItem.stories.ts
+++ src/components/atoms/IngredientItem/IngredientItem.stories.ts
@@ -11,17 +11,35 @@ const meta: Meta<typeof IngredientItem> = {
 export default meta
 type Story = StoryObj<typeof IngredientItem>
 
-export const 買い物リスト_追加済: Story = {
+export const タイトルのみ_カートボタンなし: Story = {
+  args: {
+    name: "キャベツ",
+    isUseCartButton: false,
+    isAdded: false,
+  },
+}
+
+export const タイトルと説明文_カートボタンなし: Story = {
+  args: {
+    name: "グラタン",
+    description: "作成日時: 2023年4月22日",
+    isUseCartButton: false,
+    isAdded: false,
+  },
+}
+
+export const タイトルと説明文_カートボタンあり_買い物リスト追加済: Story = {
   args: {
     name: "キャベツ",
     description: "5〜6枚",
+    isUseCartButton: true,
     isAdded: true,
   },
 }
-export const 買い物リスト_未追加: Story = {
+export const タイトルのみ_カートボタンあり_買い物リスト未追加: Story = {
   args: {
     name: "キャベツ",
-    description: "5〜6枚",
+    isUseCartButton: true,
     isAdded: false,
   },
 }
--- src/components/atoms/IngredientItem/IngredientItem.tsx
+++ src/components/atoms/IngredientItem/IngredientItem.tsx
@@ -1,5 +1,7 @@
 /**
- * 料理の材料コンポーネント ＠レシピ詳細ページ
+ * 料理の材料コンポーネント
+ * ＠レシピ詳細画面
+ * ＠マイレシピ追加画面
  */
 "use client"
 
@@ -7,11 +9,17 @@ import Image from "next/image"
 
 interface IngredientItemProps {
   name: string
-  description: string
-  isAdded: boolean // お買い物リストに追加済かどうか？
+  description?: string
+  isUseCartButton?: boolean // お買い物リストに追加ボタンを使用するか？
+  isAdded?: boolean // お買い物リストに追加済かどうか？
 }
 
-export const IngredientItem = ({ name, description, isAdded }: IngredientItemProps) => {
+export const IngredientItem = ({
+  name,
+  description,
+  isUseCartButton = false,
+  isAdded = false,
+}: IngredientItemProps) => {
   const handleClick = () => {
     if (isAdded) {
       // TODO: お買い物リストから削除する処理をかく
@@ -21,19 +29,20 @@ export const IngredientItem = ({ name, description, isAdded }: IngredientItemPro
   }
 
   return (
-    <li className="py-2 px-4 max-w-[480px] flex justify-between items-center">
+    <li className="py-2 px-4 max-w-[480px] h-[49px] flex justify-between items-center">
       <div>
         <h3 className="text-Mauve-12 text-fs14 leading-[17px]">{name}</h3>
-        <p className="text-Mauve-10 text-fs10 leading-[12px] mt-1">{description}</p>
+        {description && <p className="text-Mauve-10 text-fs10 leading-[12px] mt-1">{description}</p>}
       </div>
-
-      <button type="button" onClick={handleClick}>
-        {isAdded ? (
-          <Image src="cart_icon_red.svg" alt="削除" width={20} height={20} />
-        ) : (
-          <Image src="cart_icon_gray.svg" alt="追加" width={20} height={20} />
-        )}
-      </button>
+      {isUseCartButton && (
+        <button type="button" onClick={handleClick}>
+          {isAdded ? (
+            <Image src="cart_icon_red.svg" alt="削除" width={20} height={20} />
+          ) : (
+            <Image src="cart_icon_gray.svg" alt="追加" width={20} height={20} />
+          )}
+        </button>
+      )}
     </li>
   )
 }
