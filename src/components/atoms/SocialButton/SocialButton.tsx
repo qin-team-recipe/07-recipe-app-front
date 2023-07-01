687a1385222a633bbe85ebf697513f7e59b5cb07
Your Name <you@example.com>
2023-07-01 16:59:58 +0900

    ソーシャルボタン追加

---
+++ src/components/atoms/SocialButton/SocialButton.tsx
@@ -0,0 +1,27 @@
+import Image from "next/image"
+
+interface SocialButtonProps {
+  platform: "Google" | "Apple"
+  onClick?: () => void
+}
+
+export const SocialButton = ({ platform = "Google", onClick }: SocialButtonProps) => {
+  const renderIcon = () => {
+    let iconPath = ""
+    if (platform === "Google") iconPath = "/assets/google_icon.svg"
+    if (platform === "Apple") iconPath = "/assets/apple_icon.svg"
+
+    return <Image className="mr-1" src={iconPath} width={16} height={16} alt={`${platform}Icon`} />
+  }
+
+  return (
+    <button
+      type="button"
+      className="text-fs14 bg-Blue-light-10 text-White font-bold rounded cursor-pointer py-2 px-3 flex items-center"
+      onClick={onClick}
+    >
+      {renderIcon()}
+      {platform}でログイン
+    </button>
+  )
+}
