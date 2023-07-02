86a0132a872e4a44241e6bc414ff46376009818c
Your Name <you@example.com>
2023-07-02 23:14:04 +0900

    ボタンの背景色を修正

--- src/components/atoms/SocialLoginButton/SocialLoginButton.tsx
+++ src/components/atoms/SocialLoginButton/SocialLoginButton.tsx
@@ -14,10 +14,15 @@ export const SocialLoginButton = ({ platform = "Google", onClick }: SocialLoginB
     return <Image className="mr-1" src={iconPath} width={16} height={16} alt={`${platform}Icon`} />
   }
 
+  const buttonColor = () => {
+    if (platform === "Google") return "bg-Blue-light-10"
+    if (platform === "Apple") return "bg-Mauve-12"
+  }
+
   return (
     <button
       type="button"
-      className="text-fs14 bg-Blue-light-10 text-White font-bold rounded cursor-pointer py-2 px-3 flex items-center"
+      className={`text-fs14 ${buttonColor()} text-White font-bold rounded cursor-pointer py-2 px-3 flex items-center`}
       onClick={onClick}
     >
       {renderIcon()}
