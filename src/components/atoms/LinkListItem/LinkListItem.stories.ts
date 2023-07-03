044cfdf611ead19817653af44d5dc5cd12b53f80
RyotaS <wstone.notsw@gmail.com>
2023-07-03 20:33:35 +1000

    storybook追加

---
+++ src/components/atoms/LinkListItem/LinkListItem.stories.ts
@@ -0,0 +1,38 @@
+import { LinkListItem, LINK_TYPE } from "./LinkListItem"
+
+import type { Meta, StoryObj } from "@storybook/react"
+
+const meta: Meta<typeof LinkListItem> = {
+  title: "Atoms/LinkListItem",
+  component: LinkListItem,
+  tags: ["autodocs"],
+}
+
+export default meta
+type Story = StoryObj<typeof LinkListItem>
+
+export const TwitterLinkItem: Story = {
+  args: {
+    linkType: LINK_TYPE["TWITTER"],
+    linkTitle: "Twitter",
+    linkDiscription: "@username",
+    linkDestination: "https://twitter.com/",
+  },
+}
+
+export const InstagramLinkItem: Story = {
+  args: {
+    linkType: LINK_TYPE["INSTAGLAM"],
+    linkTitle: "Instagram",
+    linkDiscription: "@username",
+    linkDestination: "https://Instagram.com/",
+  },
+}
+export const OtherLinkItem: Story = {
+  args: {
+    linkType: LINK_TYPE["OTHER"],
+    linkTitle: "サイト",
+    linkDiscription: "https://www.google.com/",
+    linkDestination: "https://www.google.com/",
+  },
+}
---
+++ src/components/organisms/LinkTab/LinkTab.stories.ts
@@ -0,0 +1,38 @@
+import { LinkTab } from "./LinkTab"
+
+import type { Meta, StoryObj } from "@storybook/react"
+
+const meta: Meta<typeof LinkTab> = {
+  title: "Organisms/LinkTab",
+  component: LinkTab,
+  tags: ["autodocs"],
+}
+
+export default meta
+type Story = StoryObj<typeof LinkTab>
+
+export const List: Story = {
+  args: [
+    {
+      index: 14,
+      linkType: "TWITTER",
+      linkTitle: "Twitter",
+      linkDiscription: "@username",
+      linkDestination: "https://twitter.com/",
+    },
+    {
+      index: 24,
+      linkType: "INSTAGLAM",
+      linkTitle: "Instagram",
+      linkDiscription: "@username",
+      linkDestination: "https://Instagram.com/",
+    },
+    {
+      index: 34,
+      linkType: "OTHER",
+      linkTitle: "サイト",
+      linkDiscription: "https://www.google.com/",
+      linkDestination: "https://www.google.com/",
+    },
+  ],
+}
