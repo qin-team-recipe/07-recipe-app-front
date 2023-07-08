96454992a1662e8dbbdfa91cc29bd4f2aa18fa53
RyotaS <wstone.notsw@gmail.com>
2023-07-08 17:13:45 +1000

    レビュー指摘対応:DOMの出し分け処理しゅうせい

--- src/components/atoms/LinkListItem/LinkListItem.tsx
+++ src/components/atoms/LinkListItem/LinkListItem.tsx
@@ -2,31 +2,24 @@ import Image from "next/image"
 import Link from "next/link"
 
 export const LINK_TYPE = {
-  TWITTER: "TWITTER",
-  INSTAGRAM: "INSTAGRAM",
-  OTHER: "OTHER",
+  YOUTUBE: "youtube",
+  TWITTER: "twitter",
+  INSTAGRAM: "instagram",
+  OTHER: "website_link",
 } as const
 
-type LINK_TYPE = keyof typeof LINK_TYPE
+type LINK_TYPE = (typeof LINK_TYPE)[keyof typeof LINK_TYPE]
 type LinkListItemProps = Required<{
-  linkType: LINK_TYPE
+  platform: LINK_TYPE
   linkTitle: string
   linkDiscription: string
   linkDestination: string
 }>
 
-export const LinkListItem = ({ linkType, linkTitle, linkDiscription, linkDestination }: LinkListItemProps) => {
+export const LinkListItem = ({ platform, linkTitle, linkDiscription, linkDestination }: LinkListItemProps) => {
   return (
     <div className="w-[390px] h-16 px-4 py-3 border border-Mauve-06 justify-start items-center gap-4 inline-flex">
-      {(() => {
-        if (linkType === LINK_TYPE.TWITTER) {
-          return <Image src="/twitter_icon.svg" width={36} height={36} alt="twitterIcon" />
-        } else if (linkType === LINK_TYPE.INSTAGRAM) {
-          return <Image src="/instagram_icon.svg" width={36} height={36} alt="instagramIcon" />
-        } else {
-          return <Image src="/link_icon.svg" width={36} height={36} alt="linkIcon" />
-        }
-      })()}
+      <Image src={`/${platform}_icon.svg`} width={36} height={36} alt={`${platform}Icon`} />
       <div className="grow flex-col justify-start items-start gap-1 inline-flex">
         <div className="text-Mauve-12 text-fs16 font-Inter">{linkTitle}</div>
         <div className="text-Mauve-11 text-fs14 font-Inter">{linkDiscription}</div>
--- src/components/organisms/LinkTab/LinkTab.tsx
+++ src/components/organisms/LinkTab/LinkTab.tsx
@@ -3,22 +3,29 @@ import { LinkListItem, LINK_TYPE } from "../../atoms/LinkListItem/LinkListItem"
 export const LinkTab = () => {
   const dummyData = [
     {
+      index: 4,
+      platform: LINK_TYPE["YOUTUBE"],
+      linkTitle: "Twitter",
+      linkDiscription: "@username",
+      linkDestination: "https://youtube.com/",
+    },
+    {
       index: 14,
-      linkType: LINK_TYPE["TWITTER"],
+      platform: LINK_TYPE["TWITTER"],
       linkTitle: "Twitter",
       linkDiscription: "@username",
       linkDestination: "https://twitter.com/",
     },
     {
       index: 24,
-      linkType: LINK_TYPE["INSTAGRAM"],
+      platform: LINK_TYPE["INSTAGRAM"],
       linkTitle: "Instagram",
       linkDiscription: "@username",
       linkDestination: "https://Instagram.com/",
     },
     {
       index: 34,
-      linkType: LINK_TYPE["OTHER"],
+      platform: LINK_TYPE["OTHER"],
       linkTitle: "サイト",
       linkDiscription: "https://www.google.com/",
       linkDestination: "https://www.google.com/",
@@ -30,7 +37,7 @@ export const LinkTab = () => {
       {dummyData.map((v) => (
         <LinkListItem
           key={v.index}
-          linkType={v.linkType}
+          platform={v.platform}
           linkTitle={v.linkTitle}
           linkDiscription={v.linkDiscription}
           linkDestination={v.linkDestination}
