24866988d6b32e9324f6f59a39ae1025f837060b
RyotaS <wstone.notsw@gmail.com>
2023-07-08 17:48:40 +1000

    レビュー指摘対応：クリック領域が画像のみになっていた問題修正

--- src/components/atoms/LinkListItem/LinkListItem.tsx
+++ src/components/atoms/LinkListItem/LinkListItem.tsx
@@ -18,15 +18,16 @@ type LinkListItemProps = Required<{
 
 export const LinkListItem = ({ platform, linkTitle, linkDiscription, linkDestination }: LinkListItemProps) => {
   return (
-    <div className="w-[390px] h-16 px-4 py-3 border border-Mauve-06 justify-start items-center gap-4 inline-flex">
-      <Image src={`/${platform}_icon.svg`} width={36} height={36} alt={`${platform}Icon`} />
-      <div className="grow flex-col justify-start items-start gap-1 inline-flex">
-        <div className="text-Mauve-12 text-fs16 font-Inter">{linkTitle}</div>
-        {linkDiscription && <div className="text-Mauve-11 text-fs14 font-Inter">{linkDiscription}</div>}
-      </div>
-      <Link href={linkDestination}>
+    <Link href={linkDestination}>
+      <div className="w-[390px] h-16 px-4 py-3 border border-Mauve-06 justify-start items-center gap-4 inline-flex">
+        <Image src={`/${platform}_icon.svg`} width={36} height={36} alt={`${platform}Icon`} />
+        <div className="grow flex-col justify-start items-start gap-1 inline-flex">
+          <div className="text-Mauve-12 text-fs16 font-Inter">{linkTitle}</div>
+          {linkDiscription && <div className="text-Mauve-11 text-fs14 font-Inter">{linkDiscription}</div>}
+        </div>
+
         <Image className="mr-1" src="/arrow_link.svg" width={16} height={16} alt="addIcon" />
-      </Link>
-    </div>
+      </div>
+    </Link>
   )
 }
