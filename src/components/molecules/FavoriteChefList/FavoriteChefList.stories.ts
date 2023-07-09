8054926839d896b02b1b27c792e4a0350788b99c
Your Name <you@example.com>
2023-07-10 00:06:24 +0900

    10人分のシェフに変更

--- src/components/molecules/FavoriteChefList/FavoriteChefList.stories.ts
+++ src/components/molecules/FavoriteChefList/FavoriteChefList.stories.ts
@@ -21,12 +21,47 @@ export const お気に入りシェフあり: Story = {
       },
       {
         name: "田中シェフ",
-        icon: "https://picsum.photos/300",
+        icon: "https://picsum.photos/201",
+        url: "/",
+      },
+      {
+        name: "後藤シェフ",
+        icon: "https://picsum.photos/202",
         url: "/",
       },
       {
         name: "しまぶー",
-        icon: "https://picsum.photos/400",
+        icon: "https://picsum.photos/203",
+        url: "/",
+      },
+      {
+        name: "しまぶー",
+        icon: "https://picsum.photos/204",
+        url: "/",
+      },
+      {
+        name: "しまぶー",
+        icon: "https://picsum.photos/205",
+        url: "/",
+      },
+      {
+        name: "しまぶー",
+        icon: "https://picsum.photos/206",
+        url: "/",
+      },
+      {
+        name: "しまぶー",
+        icon: "https://picsum.photos/207",
+        url: "/",
+      },
+      {
+        name: "しまぶー",
+        icon: "https://picsum.photos/208",
+        url: "/",
+      },
+      {
+        name: "しまぶー",
+        icon: "https://picsum.photos/209",
         url: "/",
       },
     ],
