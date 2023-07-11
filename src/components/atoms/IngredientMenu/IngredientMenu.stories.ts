34809a3bc65577ae9ecda8fe13cf20aae9407617
Your Name <you@example.com>
2023-07-12 00:11:24 +0900

    story追加

---
+++ src/components/atoms/IngredientMenu/IngredientMenu.stories.ts
@@ -0,0 +1,34 @@
+import { IngredientMenu } from "@/components/atoms/IngredientMenu/IngredientMenu"
+
+import type { Meta, StoryObj } from "@storybook/react"
+
+const meta: Meta<typeof IngredientMenu> = {
+  title: "Atoms/IngredientMenu",
+  component: IngredientMenu,
+  tags: ["autodocs"],
+}
+
+export default meta
+type Story = StoryObj<typeof IngredientMenu>
+
+export const 全て表示: Story = {
+  args: {
+    showMoveUp: true,
+    showMoveDown: true,
+    showEdit: true,
+  },
+}
+
+export const 上に移動するのみ: Story = {
+  args: {
+    showMoveUp: true,
+    showMoveDown: false,
+  },
+}
+
+export const 下に移動するのみ: Story = {
+  args: {
+    showMoveUp: false,
+    showMoveDown: true,
+  },
+}
