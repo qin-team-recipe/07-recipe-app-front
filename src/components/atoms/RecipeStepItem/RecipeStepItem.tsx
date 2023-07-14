95a46a957ffa1b6dfd345173e134975c5e862b8d
yuktnk <yuki0wl@gmail.com>
2023-07-14 21:09:04 +0900

    feat: fs12→xs

--- src/components/atoms/RecipeStepItem/RecipeStepItem.tsx
+++ src/components/atoms/RecipeStepItem/RecipeStepItem.tsx
@@ -7,7 +7,7 @@ export interface RecipeStepItemProps {
 export const RecipeStepItem = ({ index, title, description }: RecipeStepItemProps) => {
   return (
     <li className="box-border flex max-w-[480px] list-none border-0 border-b-[1px] border-solid border-Mauve-07 px-4 py-2">
-      <p className="text-fs12 my-0 h-[18px] w-[18px] rounded-full bg-Tomato-11 text-center leading-[18px] text-Mauve-01">
+      <p className="my-0 h-[18px] w-[18px] rounded-full bg-Tomato-11 text-center text-xs leading-[18px] text-Mauve-01">
         {index}
       </p>
       <div className="ml-2 flex-1">
