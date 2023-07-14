6851c76d34eb67b584606ef20af8ea43cc860910
yuktnk <yuki0wl@gmail.com>
2023-07-14 21:23:08 +0900

    feat: fs16→base

--- src/components/molecules/IngredientList/IngredientList.tsx
+++ src/components/molecules/IngredientList/IngredientList.tsx
@@ -50,7 +50,7 @@ export const IngredientList = ({ servingOfNumber }: Props) => {
         <h3 className="text-fs20 font-bold leading-none text-Mauve-12">{servingOfNumber}人前</h3>
         <button className="flex items-center font-bold" onClick={handleClickAddAll}>
           <Image className="mr-[2px]" src="assets/icons/cart_gray.svg" alt="cartIcon" width={18} height={18} />
-          <span className="text-fs16 font-bold leading-none text-Mauve-09">まとめてお買い物に追加</span>
+          <span className="text-base font-bold text-Mauve-09">まとめてお買い物に追加</span>
         </button>
       </div>
       <ul className="divide-y divide-Mauve-07 border border-l-0 border-r-0 border-Mauve-07">
