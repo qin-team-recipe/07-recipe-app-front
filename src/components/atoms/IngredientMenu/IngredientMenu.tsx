5d4c62565a9a2f9d3101162e1190af56d3d08d29
Your Name <you@example.com>
2023-07-16 18:33:12 +0900

    不要記述削除

--- src/components/atoms/IngredientMenu/IngredientMenu.tsx
+++ src/components/atoms/IngredientMenu/IngredientMenu.tsx
@@ -1,7 +1,6 @@
-import { Menu } from "@mantine/core"
-import Image from "next/image"
-
 import { ChevronDown, ChevronUp, Pencil, Trash } from "tabler-icons-react"
+
+import { Menu } from "@mantine/core"
 
 interface IngredientMenuProps {
   showMoveUp: boolean
@@ -9,32 +8,33 @@ interface IngredientMenuProps {
   showEdit: boolean
 }
 
-export const IngredientMenu = ({ showMoveUp = true, showMoveDown = true, showEdit = true }: IngredientMenuProps) => {
+export const IngredientMenu = ({ showMoveUp, showMoveDown, showEdit }: IngredientMenuProps) => {
   const commonColor = "#6F6E77"
   return (
     <Menu position="bottom-end" width={258}>
       <Menu.Target>
+        {/* eslint-disable-next-line @next/next/no-img-element */}
         <img src="assets/icons/tabler-icon-dots-vertical.svg" alt="メニュー" width={20} height={20} />
       </Menu.Target>
 
       <Menu.Dropdown>
         {showEdit && (
-          <Menu.Item color={commonColor} icon={<Pencil color={commonColor} />}>
+          <Menu.Item color={commonColor} icon={<Pencil />}>
             編集する
           </Menu.Item>
         )}
         {showMoveUp && (
-          <Menu.Item color={commonColor} icon={<ChevronUp color={commonColor} />}>
+          <Menu.Item color={commonColor} icon={<ChevronUp />}>
             上に移動する
           </Menu.Item>
         )}
         {showMoveDown && (
-          <Menu.Item color={commonColor} icon={<ChevronDown color={commonColor} />}>
+          <Menu.Item color={commonColor} icon={<ChevronDown />}>
             下に移動する
           </Menu.Item>
         )}
         <Menu.Divider />
-        <Menu.Item color={commonColor} icon={<Trash color={commonColor} />}>
+        <Menu.Item color={commonColor} icon={<Trash />}>
           リストから削除する
         </Menu.Item>
       </Menu.Dropdown>
