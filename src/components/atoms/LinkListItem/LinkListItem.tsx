f07742ce855a00a1bd9d921e6f1091afd485a7d7
RyotaS <wstone.notsw@gmail.com>
2023-07-16 22:24:43 +1000

    fs16→base

--- src/components/atoms/LinkListItem/LinkListItem.tsx
+++ src/components/atoms/LinkListItem/LinkListItem.tsx
@@ -9,7 +9,7 @@ export const LinkListItem = ({ linkDestination }: LinkListItemProps) => {
     <Link href={linkDestination}>
       <div className="inline-flex h-16 w-[390px] items-center justify-start gap-4 border border-Mauve-06 px-4 py-3">
         <div className="inline-flex grow flex-col items-start justify-start gap-1">
-          <div className="font-Inter text-fs16 text-Mauve-12">{linkDestination}</div>
+          <div className="font-Inter text-base text-Mauve-12">{linkDestination}</div>
         </div>
       </div>
     </Link>
