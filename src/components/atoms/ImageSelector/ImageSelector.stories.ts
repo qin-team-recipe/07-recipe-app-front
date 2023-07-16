2c8a135738de71fa46d2e59f580a9530e3a48994
RyotaS <wstone.notsw@gmail.com>
2023-07-16 23:00:19 +1000

    feat :+1: : ImageSelectorコンポーネント登録
    Issue #

---
+++ src/components/atoms/ImageSelector/ImageSelector.stories.ts
@@ -0,0 +1,16 @@
+import { ImageSelector } from "./ImageSelector"
+
+import type { Meta, StoryObj } from "@storybook/react"
+
+const meta: Meta<typeof ImageSelector> = {
+  title: "Atoms/ImageSelector",
+  component: ImageSelector,
+  tags: ["autodocs"],
+}
+
+export default meta
+type Story = StoryObj<typeof ImageSelector>
+
+export const YoutubeLinkItem: Story = {
+  args: {},
+}
