57620095d8d962901d1377c34ce9921d6ba93579
yuktnk <yuki0wl@gmail.com>
2023-07-20 21:57:16 +0900

    DetailTopで使用する画像の格納先整理

--- src/components/atoms/DetailTop/DetailTop.stories.ts
+++ src/components/atoms/DetailTop/DetailTop.stories.ts
@@ -15,7 +15,7 @@ export const シェフ: Story = {
     name: "山田シェフ",
     content:
       "初の絵本出版！ 『 まねっこシェフ』 ・ふわふわ！スクランブルエッグ ・にぎにぎ！おにぎり 主婦の友社より３月３日、２冊同時発売！ 絶賛発売中！",
-    url: "detail_top_chef.jpg",
+    url: "assets/images/detail_top_chef.jpg",
   },
 }
 export const レシピ: Story = {
@@ -23,6 +23,6 @@ export const レシピ: Story = {
     name: "グラタン",
     content:
       "はじめてでも失敗なく作れるような、鶏肉や玉ねぎを具とした基本的なマカロニグラタンのレシピです。ソースと具材炒めを別器具で行うレシピも多いですが、グラタンの具を炒めたフライパンの中で、そのままホワイトソースを仕上げる手軽な作り方にしています。ぜひお試しください。",
-    url: "detail_top_gratin.jpg",
+    url: "assets/images/detail_top_gratin.jpg",
   },
 }
