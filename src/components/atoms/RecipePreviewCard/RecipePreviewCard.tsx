d9788ea491b85d892538de14431c8cab2c104f2c
RyotaS <wstone.notsw@gmail.com>
2023-07-22 01:03:07 +1000

    レシピカードコンポーネント幅調整

--- src/components/atoms/RecipePreviewCard/RecipePreviewCard.tsx
+++ src/components/atoms/RecipePreviewCard/RecipePreviewCard.tsx
@@ -23,7 +23,7 @@ type RecipePreviewCardProps = Required<{
 export const RecipePreviewCard = ({ id, imagePath, previewTitle, previewContent, size }: RecipePreviewCardProps) => {
   return (
     <div className={`w-[${PIC_SIZE[size]}px] h-[${H_SIZE[size]}px]`}>
-      <Image width={PIC_SIZE[size]} height={PIC_SIZE[size]} src={imagePath} alt={`レシピ画像${index}`}></Image>
+      <Image width={PIC_SIZE[size]} height={PIC_SIZE[size]} src={imagePath} alt={`レシピ画像${id}`}></Image>
       <p className="my-2 line-clamp-2 text-xs font-bold text-Mauve-12">{previewTitle}</p>
       <p className="line-clamp-1 text-xs font-normal text-Mauve-11">{previewContent}</p>
     </div>
