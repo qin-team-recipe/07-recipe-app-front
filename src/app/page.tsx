5477e32ff9c0591df5929caae608a0dd49a9c5ad
RyotaS <wstone.notsw@gmail.com>
2023-07-22 15:54:22 +1000

    検索画面にもっと見るボタンを表示させる

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import { SearchBar } from "@/components/atoms/SearchBar/SearchBar"
 import { SectionTitle } from "@/components/atoms/SectionTitle/SectionTitle"
+import { LoadMoreButton } from "@/components/atoms/buttons/LoadMoreButton/LoadMoreButton"
 import { FeatureChefList } from "@/components/molecules/FeatureChefList/FeatureChefList"
 import { OtherChefList } from "@/components/molecules/OtherChefList/OtherChefList"
 import { RecipeList } from "@/components/molecules/RecipeList/RecipeList"
@@ -14,13 +15,23 @@ export default function SearchPage() {
         <SectionTitle text="注目のシェフ" fontSize="xl" heading="h1" />
         <FeatureChefList />
       </section>
-      <section className="mb-10 grid gap-4">
+      <section className="mb-10 grid grid-cols-2 gap-4">
         <SectionTitle text="話題のレシピ" fontSize="xl" heading="h1" />
-        <RecipeList horizontalScroll />
+        <div className="flex justify-end">
+          <LoadMoreButton />
+        </div>
+        <div className="col-span-2">
+          <RecipeList horizontalScroll />
+        </div>
       </section>
-      <section className="mb-10 grid gap-4">
+      <section className="mb-10 grid grid-cols-2 gap-4">
         <SectionTitle text="シェフ" fontSize="xl" heading="h1" />
-        <OtherChefList />
+        <div className="flex justify-end">
+          <LoadMoreButton />
+        </div>
+        <div className="col-span-2">
+          <OtherChefList />
+        </div>
       </section>
     </>
   )
