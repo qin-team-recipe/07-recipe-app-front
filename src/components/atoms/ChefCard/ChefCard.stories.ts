3c7e106442b535a9cfd609e347f998dbc857df9d
kyoichi <s206cg06k@yahoo.co.jp>
2023-07-22 17:53:47 +0900

    修正：マップの削除、propsにてデータの受け渡しができるように変更、不要なflex、rounded等を削除

--- src/components/atoms/ChefCard/ChefCard.stories.ts
+++ src/components/atoms/ChefCard/ChefCard.stories.ts
@@ -11,6 +11,21 @@ const meta: Meta<typeof ChefCard> = {
 export default meta
 type Story = StoryObj<typeof ChefCard>
 
-export const Normal: Story = {
-  args: {}
+export const 説明３行: Story = {
+  args: {
+    icon: "detail_hifumiChef.svg",
+    name: "山田シェフ",
+    text: "白ごはん.comを運営しています。アップしたレシピの紹介や、youtube動画、日々の食のこと、オリジナル商品などの案内等をブログでやっています。",
+    args: "/",
+    count: 123,
+  },
+}
+export const 説明１行: Story = {
+  args: {
+    icon: "detail_hifumiChef.svg",
+    name: "山田シェフ",
+    text: "白ごはん.comを運営しています。",
+    args: "/",
+    count: 123,
+  },
 }
--- src/components/atoms/ChefCard/ChefCard.tsx
+++ src/components/atoms/ChefCard/ChefCard.tsx
@@ -1,32 +1,26 @@
 import Image from "next/image"
 import Link from "next/link"
 
-const ChefCardItems = [
-  {
-    icon: "detail_hifumiChef.svg",
-    name: "山田シェフ",
-    text: "白ごはん.comを運営しています。アップしたレシピの紹介や、youtube動画、日々の食のこと、オリジナル商品などの案内等をブログでやっています。",
-    url: "/",
-    count: 123,
-  },
-]
+type ChefCardProps = {
+  icon: string
+  name: string
+  text: string
+  args: string
+  count: number
+}
 
-export const ChefCard = () => {
+export const ChefCard = (props: ChefCardProps) => {
   return (
-    <div>
-      {ChefCardItems.map((item) => (
-        <Link key={item.name} href={item.url} className="flex gap-4">
-          <Image src={item.icon} width={88} height={116} alt="kitchenIcon" className="rounded-2xl" />
-          <div className="h-14 w-64 text-sm">
-            <h1 className="text-lg font-bold">{item.name}</h1>
-            <p className="my-1 line-clamp-3  text-Mauve-10">{item.text}</p>
-            <div className="flex">
-              <Image src="/assets/icons/kitchen.svg" width={16} height={16} alt="kitchenIcon" className="rounded-2xl" />
-              <p className="ml-2 flex">{item.count}レシピ</p>
-            </div>
-          </div>
-        </Link>
-      ))}
-    </div>
+    <Link href={props.args} className="flex gap-4">
+      <Image src={props.icon} width={88} height={116} alt="kitchenIcon" className="rounded-2xl" />
+      <div className="h-14 w-64 text-sm">
+        <h1 className="text-lg font-bold">{props.name}</h1>
+        <p className="my-1 line-clamp-3 h-14 text-Mauve-10">{props.text}</p>
+        <div className="flex">
+          <Image src="/assets/icons/kitchen.svg" width={16} height={16} alt="kitchenIcon" />
+          <p className="ml-2">{props.count}レシピ</p>
+        </div>
+      </div>
+    </Link>
   )
 }
