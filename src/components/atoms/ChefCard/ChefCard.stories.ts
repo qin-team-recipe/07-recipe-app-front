c36468d1adc95b308efada313df116597746b5a4
kyoichi <s206cg06k@yahoo.co.jp>
2023-07-25 17:35:07 +0900

    propsの命名変更、Imageのalt変更

--- src/components/atoms/ChefCard/ChefCard.stories.ts
+++ src/components/atoms/ChefCard/ChefCard.stories.ts
@@ -13,19 +13,20 @@ type Story = StoryObj<typeof ChefCard>
 
 export const 説明３行: Story = {
   args: {
-    icon: "detail_hifumiChef.svg",
+    image: "detail_hifumiChef.svg",
     name: "山田シェフ",
-    text: "白ごはん.comを運営しています。アップしたレシピの紹介や、youtube動画、日々の食のこと、オリジナル商品などの案内等をブログでやっています。",
-    args: "/",
-    count: 123,
+    profile:
+      "白ごはん.comを運営しています。アップしたレシピの紹介や、youtube動画、日々の食のこと、オリジナル商品などの案内等をブログでやっています。",
+    url: "/",
+    numberOfRecipes: 123,
   },
 }
 export const 説明１行: Story = {
   args: {
-    icon: "detail_hifumiChef.svg",
+    image: "detail_hifumiChef.svg",
     name: "山田シェフ",
-    text: "白ごはん.comを運営しています。",
-    args: "/",
-    count: 123,
+    profile: "白ごはん.comを運営しています。",
+    url: "/",
+    numberOfRecipes: 123,
   },
 }
--- src/components/atoms/ChefCard/ChefCard.tsx
+++ src/components/atoms/ChefCard/ChefCard.tsx
@@ -2,23 +2,23 @@ import Image from "next/image"
 import Link from "next/link"
 
 type ChefCardProps = {
-  icon: string
+  image: string
   name: string
-  text: string
-  args: string
-  count: number
+  profile: string
+  url: string
+  numberOfRecipes: number
 }
 
 export const ChefCard = (props: ChefCardProps) => {
   return (
-    <Link href={props.args} className="flex gap-4">
-      <Image src={props.icon} width={88} height={116} alt="kitchenIcon" className="rounded-2xl" />
+    <Link href={props.url} className="flex gap-4">
+      <Image src={props.image} width={88} height={116} alt="シェフ画像" className="rounded-2xl" />
       <div className="h-14 w-64 text-sm">
         <h1 className="text-lg font-bold">{props.name}</h1>
-        <p className="my-1 line-clamp-3 h-14 text-Mauve-10">{props.text}</p>
+        <p className="my-1 line-clamp-3 h-14 text-Mauve-10">{props.profile}</p>
         <div className="flex">
           <Image src="/assets/icons/kitchen.svg" width={16} height={16} alt="kitchenIcon" />
-          <p className="ml-2">{props.count}レシピ</p>
+          <p className="ml-2">{props.numberOfRecipes}レシピ</p>
         </div>
       </div>
     </Link>
