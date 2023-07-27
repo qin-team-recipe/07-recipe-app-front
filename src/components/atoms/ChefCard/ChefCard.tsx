05e1c7fb550d20a3f980cc6ed6738949b6f7671a
kyoichi <s206cg06k@yahoo.co.jp>
2023-07-27 21:39:46 +0900

    修正しました

--- src/components/atoms/ChefCard/ChefCard.tsx
+++ src/components/atoms/ChefCard/ChefCard.tsx
@@ -13,9 +13,9 @@ export const ChefCard = (props: ChefCardProps) => {
   return (
     <Link href={props.url} className="flex gap-4">
       <Image src={props.image} width={88} height={116} alt="シェフ画像" className="rounded-2xl" />
-      <div className="h-14 w-64 text-sm">
+      <div className=" flex w-64 flex-col justify-between text-sm">
         <h1 className="text-lg font-bold">{props.name}</h1>
-        <p className="my-1 line-clamp-3 h-14 text-Mauve-10">{props.profile}</p>
+        <p className=" line-clamp-3  h-full text-Mauve-10">{props.profile}</p>
         <div className="flex">
           <Image src="/assets/icons/kitchen.svg" width={16} height={16} alt="kitchenIcon" />
           <p className="ml-2">{props.numberOfRecipes}レシピ</p>
