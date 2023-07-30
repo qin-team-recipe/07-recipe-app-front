5e849fefc95682bf980875f839fafa5fd6375ac0
Your Name <you@example.com>
2023-07-30 20:43:43 +0900

    レシピモーダル実装

--- src/components/atoms/RecipeStepItem/RecipeStepItem.tsx
+++ src/components/atoms/RecipeStepItem/RecipeStepItem.tsx
@@ -1,13 +1,17 @@
 export interface RecipeStepItemProps {
   index: number
   text: string
+  handleOpenModal?: () => void
 }
 
-export const RecipeStepItem = ({ index, text }: RecipeStepItemProps) => {
+export const RecipeStepItem = ({ index, text, handleOpenModal }: RecipeStepItemProps) => {
   return (
-    <li className="box-border flex max-w-[480px] list-none border-0 border-b-[1px] border-solid border-Mauve-07 px-4 py-2">
+    <li
+      onClick={handleOpenModal}
+      className="box-border flex max-w-[480px] list-none border-0 border-b-[1px] border-solid border-Mauve-07 px-4 py-2"
+    >
       <p className=" h-[20px] w-[20px] rounded-full bg-Tomato-11 text-center text-sm text-Mauve-01">{index}</p>
-      <p className="flex-1 ml-2 text-sm text-Mauve-12">{text}</p>
+      <p className="ml-2 flex-1 text-sm text-Mauve-12">{text}</p>
     </li>
   )
 }
