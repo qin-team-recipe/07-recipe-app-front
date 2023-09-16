e05a8d331030d79766bda632613a271dac92bc25
kyoichi <s206cg06k@yahoo.co.jp>
2023-09-16 15:01:47 +0900

    feature/createComponent/atoms/InputText　を作成

---
+++ src/components/atoms/InputText/InputText.tsx
@@ -0,0 +1,31 @@
+"use client"
+import { useState } from "react"
+
+export type InputTextProps = {
+  placeholder: string
+}
+
+export const InputText = ({ placeholder }: InputTextProps) => {
+  const [text, setText] = useState("")
+  const handleInputTextChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    e.preventDefault()
+    setText(e.target.value)
+  }
+  // 下記クリア処理は今のところ不要かも？
+  // const clearInputText = () => {
+  //   setText("")
+
+  return (
+    <>
+      <div className="pb-5">
+        <input
+          type="text"
+          className="w-full border-y-[1px]  border-solid border-Mauve-07 px-4 py-2  placeholder-Mauve-07 outline-none"
+          placeholder={placeholder}
+          value={text}
+          onChange={handleInputTextChange}
+        />
+      </div>
+    </>
+  )
+}
