30a400f0cdb22e45e50af328061753ace6ce9201
kyoichi <s206cg06k@yahoo.co.jp>
2023-10-15 17:13:58 +0900

    アイコンの名前変更による修正

--- src/components/atoms/buttons/CopyButton/CopyButton.tsx
+++ src/components/atoms/buttons/CopyButton/CopyButton.tsx
@@ -7,7 +7,7 @@ type Props = {
 export const CopyButton = ({ onClick }: Props) => {
   return (
     <button className="ml-auto mr-0 flex items-center px-4 py-2" onClick={onClick}>
-      <Image className="mr-[2px]" src="assets/icons/copy.svg" alt="copyIcon" width={18} height={18} />
+      <Image className="mr-[2px]" src="assets/icons/copy_blue.svg" alt="copyIcon" width={18} height={18} />
       <span className="text-xs text-Blue-alpha-11">コピーする</span>
     </button>
   )
