e06fa2547456f796598a1cc1fbcd99f912344003
kyoichi <s206cg06k@yahoo.co.jp>
2023-10-15 10:13:22 +0900

    軽微な修正（SectionTitlePropsの型定義をオプショナルに変更）

--- src/components/organisms/RecipeTab/RecipeTab.tsx
+++ src/components/organisms/RecipeTab/RecipeTab.tsx
@@ -2,7 +2,7 @@ import { SectionTitle } from "@/components/atoms/SectionTitle/SectionTitle"
 import { RecipeList } from "@/components/molecules/RecipeList/RecipeList"
 
 type SectionTitleProps = {
-  text: string
+  text?: string
 }
 
 export const RecipeTab = (props: SectionTitleProps) => {
